e474ddf6670d6e21e9f7852aea461815862bcca7
ARMSTRONGDGHOST <motanbouarmstrong@gmail.com>
2024-10-07 19:35:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID |𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=H6RCFTQR#jERMMDHi8kq7zk_s77F1X0R1qZgN_N1m41LihH3MyL4| "",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
